a3391033d457b1575d207c12e9a6848b560b3719
FantasticFiasco <mattias.kindborg@gmail.com>
2018-11-03 09:23:45 +0100

    wip

--- in-memory/src/InMemoryEventStore.cs
+++ in-memory/src/InMemoryEventStore.cs
@@ -17,56 +17,43 @@ namespace InMemory
 
         public IEnumerable LoadEventsFor<TAggregate>(Guid id)
         {
-            // Get the current event stream; note that we never mutate the
-            // Events array so it's safe to return the real thing.
-            Stream s;
-
-            if (store.TryGetValue(id, out s))
-            {
-                return s.Events;
-            }
-            else
-            {
-                return new ArrayList();
-            }
+            // Get the current event stream. Note that we never mutate the
+            // events array so it's safe to return the real thing.
+            return store.TryGetValue(id, out var stream)
+                ? stream.Events
+                : new ArrayList();
         }
 
         public void SaveEventsFor<TAggregate>(Guid aggregateId, int eventsLoaded, ArrayList newEvents)
         {
-            // Get or create stream.
-            var s = store.GetOrAdd(aggregateId, _ => new Stream());
+            // Get or create stream
+            var stream = store.GetOrAdd(aggregateId, _ => new Stream());
 
-            // We'll use a lock-free algorithm for the update.
+            // We'll use a lock-free algorithm for the update
             while (true)
             {
-                // Read the current event list.
-                var eventList = s.Events;
+                // Read the current event list
+                var events = stream.Events;
 
-                // Ensure no events persisted since us.
-                var prevEvents = eventList == null ? 0 : eventList.Count;
-                if (prevEvents != eventsLoaded)
-                    throw new Exception("Concurrency conflict; cannot persist these events");
+                // Ensure no events persisted since us
+                var previousEvents = events?.Count ?? 0;
+                
+                if (previousEvents != eventsLoaded) throw new Exception("Concurrency conflict; cannot persist these events");
 
                 // Create a new event list with existing ones plus our new
                 // ones (making new important for lock free algorithm!)
-                var newEventList = eventList == null
+                var newEventList = events == null
                     ? new ArrayList()
-                    : (ArrayList)eventList.Clone();
+                    : (ArrayList)events.Clone();
+                
                 newEventList.AddRange(newEvents);
 
-                // Try to put the new event list in place atomically.
-                if (Interlocked.CompareExchange(ref s.Events, newEventList, eventList) == eventList)
+                // Try to put the new event list in place atomically
+                if (Interlocked.CompareExchange(ref stream.Events, newEventList, events) == events)
+                {
                     break;
+                }
             }
-        }
-
-        private Guid GetAggregateIdFromEvent(object e)
-        {
-            var idField = e.GetType().GetField("Id");
-
-            if (idField == null) throw new Exception("Event type " + e.GetType().Name + " is missing an Id field");
-
-            return (Guid)idField.GetValue(e);
         }
 
         private class Stream
