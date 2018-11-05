ece3c6ce093ecaabafb66ae26af3517fb5e3dd7f
FantasticFiasco <mattias.kindborg@gmail.com>
2018-11-05 23:14:01 +0100

    wip

---
+++ src/todo-readmodel/TodoList.cs
@@ -0,0 +1,23 @@
+﻿using Cqrs;
+using Todo.Events;
+
+namespace Todo.ReadModel
+{
+    public class TodoList :
+        ISubscribeTo<TodoAdded>,
+        ISubscribeTo<TodoRenamed>,
+        ISubscribeTo<TodoCompleted>,
+        ISubscribeTo<TodoIncompleted>,
+        ISubscribeTo<TodoRemoved>
+    {
+        public void Handle(TodoAdded e) => throw new System.NotImplementedException();
+
+        public void Handle(TodoRenamed e) => throw new System.NotImplementedException();
+
+        public void Handle(TodoCompleted e) => throw new System.NotImplementedException();
+
+        public void Handle(TodoIncompleted e) => throw new System.NotImplementedException();
+
+        public void Handle(TodoRemoved e) => throw new System.NotImplementedException();
+    }
+}
