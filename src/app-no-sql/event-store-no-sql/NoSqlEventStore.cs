aab94c02dfea5f61fdac39037cd92c29fd9d77c4
FantasticFiasco <mattias.kindborg@gmail.com>
2018-12-23 00:53:44 +0100

    wip

--- src/app-no-sql/event-store-no-sql/NoSqlEventStore.cs
+++ src/app-no-sql/event-store-no-sql/NoSqlEventStore.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using Cqrs;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace EventStore.NoSql
 {
     public class NoSqlEventStore : IEventStore
     {
-        public IEnumerable<object> LoadEventsFor<TAggregate>(Guid id) => throw new NotImplementedException();
+        private readonly IMongoDatabase database;
+        private readonly BsonDocument filter;
+
+        public NoSqlEventStore()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            database = client.GetDatabase("event");
+            filter = new BsonDocument();
+        }
+
+
+        public IEnumerable<object> LoadEventsFor<TAggregate>(Guid id)
+        {
+            var collection = database.GetCollection<BsonDocument>(id.ToString());
+
+            // TODO: The items returned are unordered, please fix
+            return collection.Find(filter).ToList();
+        }
 
         public void SaveEventsFor<TAggregate>(Guid id, int eventsLoaded, object[] newEvents) => throw new NotImplementedException();
     }
