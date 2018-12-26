56452a44640fe8143627de78f9a9fbffb75911cb
FantasticFiasco <mattias.kindborg@gmail.com>
2018-12-26 11:16:21 +0100

    wip

--- src/app-no-sql/event-store-no-sql/Event.cs
+++ src/app-no-sql/event-store-no-sql/Event.cs
@@ -3,14 +3,29 @@ using MongoDB.Bson;
 
 namespace EventStore.NoSql
 {
+    /// <summary>
+    /// Class describing a event document stored in a NoSQL database.
+    /// </summary>
     public class Event
     {
+        /// <summary>
+        /// Gets or sets the version of the event.
+        /// </summary>
         public int Version { get; set; }
 
+        /// <summary>
+        /// Gets or sets the assembly qualified type name of the event.
+        /// </summary>
         public string Type { get; set; }
 
+        /// <summary>
+        /// Gets or sets the event body, i.e. the actual event published from the aggregate.
+        /// </summary>
         public BsonDocument Body { get; set; }
 
+        /// <summary>
+        /// Gets or sets the time the event was saved to the database.
+        /// </summary>
         public DateTime CreatedAt { get; set; }
     }
 }
