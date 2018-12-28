37d19e3952ba4744f738f0d9566e76a957e87200
FantasticFiasco <mattias.kindborg@gmail.com>
2018-12-28 18:03:36 +0100

    wip

--- src/apps/app-sql/frontend/Startup.cs
+++ src/apps/app-sql/frontend/Startup.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using GraphQL;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Todo.GraphQL;
 
 namespace Todo.Web
 {
