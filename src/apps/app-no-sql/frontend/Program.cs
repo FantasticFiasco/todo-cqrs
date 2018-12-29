0f22a020a0c4439cf3c44e9226663fef4552e7eb
FantasticFiasco <mattias.kindborg@gmail.com>
2018-12-29 19:09:17 +0100

    wip

--- src/apps/app-no-sql/frontend/Program.cs
+++ src/apps/app-no-sql/frontend/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace Todo.Web
+namespace Frontend
 {
     public class Program
     {
--- src/apps/app-no-sql/frontend/Startup.cs
+++ src/apps/app-no-sql/frontend/Startup.cs
@@ -1,10 +1,10 @@
-﻿using GraphQL;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Web.GraphQL;
 
-namespace Todo.Web
+namespace Frontend
 {
     public class Startup
     {
