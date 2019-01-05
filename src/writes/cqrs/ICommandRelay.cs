e0e5b56afdbfba3ad3feef305c408a5acabf4eaf
FantasticFiasco <mattias.kindborg@gmail.com>
2019-01-05 01:05:12 +0100

    wip

--- src/writes/cqrs/ICommandRelay.cs
+++ src/writes/cqrs/ICommandRelay.cs
@@ -18,8 +18,8 @@ namespace Cqrs
             where TAggregate : Aggregate, new();
 
         /// <summary>
-        /// Registers an aggregate as being the handler for all its implementations of
-        /// <see cref="IHandleCommand{T}"/>.
+        /// Registers an aggregate as being the handler for commands defined by all its
+        /// implementations of <see cref="IHandleCommand{T}"/>.
         /// </summary>
         /// <typeparam name="TAggregate">
         /// The type of the aggregate handling commands.
@@ -28,10 +28,16 @@ namespace Cqrs
             where TAggregate : Aggregate, new();
 
         /// <summary>
-        /// TODO: Rewrite
-        ///
-        /// Adds an object that subscribes to the specified event, by virtue of implementing the
-        /// <see cref="IPublisher{TEvent}"/> interface.
+        /// Registers a publisher for a particular event produced by a command invocation.
+        /// <para />
+        /// Please note that the preferred architecture is to use an event store that has the
+        /// capability of publishing events, or at least provide a stream of events a publisher can
+        /// read from. The negative aspect of registering a publisher using this method is that
+        /// persisting and publishing an event are two separate operations, not protected by a
+        /// transaction, and thus not being atomic in nature. The application might terminate after
+        /// the event has been persisted but before the event has been published, leading to a
+        /// forever inconsistent read model. Only use this method if that scenario is acceptable
+        /// according to your requirements.
         /// </summary>
         void RegisterPublisherFor<TEvent>(IPublisher<TEvent> publisher);
 
@@ -49,9 +55,6 @@ namespace Cqrs
         /// <typeparam name="TCommand">
         /// The type of the command.
         /// </typeparam>
-        /// <exception cref="Exception">
-        /// No registered handler for command is found.
-        /// </exception>
         void SendCommand<TCommand>(TCommand command);
     }
 }
